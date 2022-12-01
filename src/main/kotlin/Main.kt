12e27b1da5a9f77d69fbdeaae12ef91dca12fa6e
Michael Lesniak <mail@mlesniak.com>
2022-12-01 06:25:41 +0100

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package com.mlesniak.changeme
+
+fun main() {
+    println("Hello, world")
+}
---
+++ src/test/kotlin/MainTest.kt
@@ -0,0 +1,11 @@
+package com.mlesniak.changeme
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class MainTest {
+    @Test
+    fun test() {
+        assertTrue(true)
+    }
+}
