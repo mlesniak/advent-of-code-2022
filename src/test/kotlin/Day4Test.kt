4a8582411e357d1948eef070f54bb4f4744b5b22
Michael Lesniak <mail@mlesniak.com>
2022-12-04 20:53:35 +0100

    Day 4 part 2 / test

--- src/test/kotlin/Day4Test.kt
+++ src/test/kotlin/Day4Test.kt
@@ -1,6 +1,7 @@
 package com.mlesniak.changeme
 
 import org.junit.jupiter.api.Test
+import org.junit.platform.engine.support.discovery.SelectorResolver.Match.partial
 import java.lang.Integer.max
 import java.lang.Integer.min
 import java.nio.file.Files
@@ -35,8 +36,32 @@ class Day4Test {
         return this.first >= other.first && this.last <= other.last
     }
 
+    private fun IntRange.partial(other: IntRange): Boolean {
+        return this.last >= other.first && this.first <= other.first
+    }
+
     @Test
     fun part2() {
+        val lines = Files
+            .readAllLines(Path.of("4.txt"))
+            .map { it.split(",") }
+            .map {
+                it.map { range ->
+                    val parts = range.split("-")
+                    val start = parts[0].toInt()
+                    val end = parts[1].toInt()
+                    start..end
+                }
+            }
+            .filter { partial(it) }
+        println(lines)
+    }
+
+    private fun partial(ranges: List<IntRange>): Boolean {
+        val a = ranges[0]
+        val b = ranges[1]
+
+        return a.partial(b) || b.partial(a)
     }
 }
 
