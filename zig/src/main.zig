5ca8b2753e72dc497c95f04027e68bd16440f27d
Michael Lesniak <mail@mlesniak.com>
2023-01-15 21:23:44 +0100

    Introduce Zig language

---
+++ zig/src/main.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+
+// 498,4 -> 498,6 -> 496,6
+// 503,4 -> 502,4 -> 502,9 -> 494,9
+
+pub fn main() !void {
+    // read file 
+    // into data structure
+    // simulate algorithm
+    // tests?
+}
