6718040f7e24cc095f91515feeb08c941d8928d1
Michael Lesniak <mail@mlesniak.com>
2023-01-18 06:30:27 +0100

    Fix type error

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
-const print = std.debug.print;
 const mem = std.mem;
 const fs = std.fs;
+const print = std.debug.print;
 
 // 498,4 -> 498,6 -> 496,6
 // 503,4 -> 502,4 -> 502,9 -> 494,9
@@ -11,22 +11,33 @@ pub fn main() !void {
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
-    const buf = try readFromFile(allocator, "14.txt");
-    defer allocator.free(buf);
-
-    var lines = try splitString(allocator, buf);
+    var lines = try readLinesFromFile(allocator, "14.txt");
     for (lines.items) |line| {
-        print("Line: {s}\n", .{line});
+        print("{s}\n", .{line});
     }
 
-    for (lines.items) |line| {
-        allocator.free(line);
-    }
-    lines.clearAndFree();
+    freeLines(allocator, &lines);
 
     // into data structure
     // simulate algorithm
     // tests?
+}
+
+// A rule of thumb for returning an ArrayList vs. a slice: If you expect the caller to be adding items 
+// the list after calling the function, return an ArrayList. If you expect them to simply look through the list, or simply modify some items in it, return a slice.
+fn readLinesFromFile(allocator: std.mem.Allocator, filename: []const u8) !std.ArrayList([]const u8) {
+    const buf = try readFromFile(allocator, filename);
+    defer allocator.free(buf);
+
+    var lines = try splitString(allocator, buf);
+    return lines;
+}
+
+fn freeLines(allocator: std.mem.Allocator, lines: *std.ArrayList([]const u8)) void {
+    for (lines.items) |line| {
+        allocator.free(line);
+    }
+    lines.clearAndFree();
 }
 
 // Returned value has to be free'd by caller.
@@ -55,15 +66,15 @@ fn splitString(allocator: std.mem.Allocator, string: []u8) !std.ArrayList([]cons
             continue;
         }
 
-        var l = try allocator.alloc(u8, i - 1 - s);
-        mem.copy(u8, l, string[s..(i - 1)]);
+        var l = try allocator.alloc(u8, i - s);
+        mem.copy(u8, l, string[s..i]);
         try lines.append(l);
         s = i + 1;
         continue;
     }
 
-    var l = try allocator.alloc(u8, i - 1 - s);
-    mem.copy(u8, l, string[s..(i - 1)]);
+    var l = try allocator.alloc(u8, i - s);
+    mem.copy(u8, l, string[s..i]);
     try lines.append(l);
 
     return lines;
