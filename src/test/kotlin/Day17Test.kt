428fc98a1555343219cf89d4e112c72ed8bc8f4a
Michael Lesniak <mail@mlesniak.com>
2023-06-29 23:46:36 +0200

    Working test case

--- src/test/kotlin/Day17Test.kt
+++ src/test/kotlin/Day17Test.kt
@@ -32,21 +32,76 @@ data class Block(
     }
 
     fun down(): Block {
-        return copy(y = y + 1)
+        return copy(y = y - 1)
     }
 }
 
-
 class Area {
     private val width = 7
-    private val grid = mutableListOf<String>()
 
-    fun validate(block: Block): Boolean {
+    // Not private for debugging and hacking around.
+    val grid = mutableListOf<String>()
+
+    fun isValid(block: Block): Boolean {
+        // Hit floor?
+        if (block.y - block.rows.size + 1 < 0) {
+            return false
+        }
+
+        // Check for out of bounds.
+        var outOfOBounds = false
+        if (block.x < 0) {
+            outOfOBounds = true
+        }
+        block.rows.forEachIndexed { index, s ->
+            if (block.x + s.length > width) {
+                outOfOBounds = true
+            }
+        }
+        if (outOfOBounds) {
+            return false
+        }
+
+        // 3 ...#...
+        // 2 ..###..
+        // 1 ...#...
+        // 0 ..####.
+
+        // Check for other blocks at the same positions.
+        // println("---------- Checking block at ${block.x}, ${block.y}")
+        block.rows.forEachIndexed { yidx, row ->
+            // println("*** Checking row $yidx")
+            if (grid.size <= block.y - yidx) {
+                // We are outside the grid for this row.
+                // println("Outside grid (${grid.size}")
+                return@forEachIndexed
+            }
+            // println("Checking row ${grid[block.y - yidx]}")
+            row.forEachIndexed { xidx, c ->
+                if (c == '.') {
+                    // Can be ignored.
+                    // println("Ignoring char $c at ${block.x + xidx}, ${block.y + yidx}")
+                    return@forEachIndexed
+                }
+                // println("Checking char $c at ${block.x + xidx}, ${block.y + yidx}")
+                if (grid[block.y-yidx][block.x + xidx] == '#') {
+                    // println("Found rock at ${block.x + xidx}, ${block.y + yidx}")
+                    return false
+                }
+            }
+        }
+
         return true
     }
 
+    fun copy(): Area {
+        val area = Area()
+        area.grid.addAll(grid)
+        return area
+    }
+
     fun store(block: Block) {
-        while (grid.size <= block.y ) {
+        while (grid.size <= block.y + block.rows.size) {
             grid.add(".".repeat(width))
         }
         block.rows.forEachIndexed { index, s ->
@@ -58,27 +113,81 @@ class Area {
     fun print() {
         grid.reversed().forEachIndexed { idx, row: String ->
             val k = String.format("%2d", grid.size - idx - 1)
-            println("$k $row") }
+            println("$k $row")
+        }
+    }
+
+    fun nextY(): Int {
+        // Find topmost row with a rock.
+        for (idx in grid.indices.reversed()) {
+            if (grid[idx].contains("#")) {
+                return idx
+            }
+        }
+
+        return -1
     }
 }
 
 class Day17Test {
+    private val blocks = listOf(
+        listOf("@@@@"),
+        listOf(".@.", "@@@", ".@."),
+        listOf("..@", "..@", "@@@"),
+        listOf("@", "@", "@", "@"),
+        listOf("@@", "@@"),
+    )
+
     @Test
     fun part1() {
         val movements = Files.readString(Path.of("17.txt"))
             .map { Movement.valueOf(it) }
-            // .also { println(it) }
+        // .also { println(it) }
 
         val area = Area()
+        var movementIndex = 0
+        var blockIndex = 0
+        // area.grid.add("..####.")
 
-        val block = Block(0, 0, listOf("@@@@")).also {
-            area.store(it)
-            area.print()
+        // var block = Block(2, 4, listOf("@@@@", "..@@", "...@"))
+        var numRocks = 0
+
+        val steps = 2022
+        while (numRocks++ < steps) {
+            val nextBlock = blocks[blockIndex]
+            val nextY = area.nextY() + nextBlock.size + 3
+            var block = Block(2, nextY, nextBlock)
+            blockIndex = (blockIndex + 1) % blocks.size
+
+            // println("------------------ $numRocks")
+            // println("grid size = ${area.grid.size}")
+            // val a = area.copy()
+            // a.store(block)
+            // a.print()
+            // println()
+
+            while (true) {
+                // Move left or right.
+                // println("Applying movement ${movements[movementIndex]}")
+                val b1 = block.apply(movements[movementIndex])
+                movementIndex = (movementIndex + 1) % movements.size
+                if (area.isValid(b1)) {
+                    block = b1
+                }
+
+                // Move down.
+                // println("Applying movement down")
+                val b2 = block.down()
+                if (area.isValid(b2)) {
+                    block = b2
+                } else {
+                    break
+                }
+            }
+
+            area.store(block)
         }
-        println("---")
-        Block(0, 4, listOf("@@@@", ".@@")).also {
-            area.store(it)
-            area.print()
-        }
+        // area.print()
+        println(area.nextY() + 1)
     }
 }
