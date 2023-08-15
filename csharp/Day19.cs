1917dc457731141f569905e94382799c810ebf29
Michael Lesniak <mail@mlesniak.com>
2023-08-15 20:26:06 +0200

    Move files

---
+++ csharp/Day19.cs
@@ -0,0 +1,13 @@
+namespace Lesniak.AdventOfCode2022;
+
+internal class Day19
+{
+    public void Run()
+    {
+        var lines = File.ReadLines("19.txt");
+        foreach (string line in lines)
+        {
+            Console.WriteLine("line = {0}", line); 
+        }
+    } 
+}
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lesniak.AdventOfCode2022;
+
+new Day19().Run();
